aee0b0c59da0b004a5bbc5466454b5a4465c093e
Fedeherrera <fedeeherrera2001@gmail.com>
2023-03-01 01:30:18 -0300

    Creacion app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+    </div>
+  )
+}
+
+export default App
